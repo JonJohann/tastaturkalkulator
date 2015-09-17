bbc676061740ca10179825a162b601ac17ce6752
Rune Holmdal <rholmdfal@gmail.com>
2015-09-17 11:18:42 +0200

    Tastatur inntasting i tillegg til skjerm
    Ikke komplett, men fungerende og danner basis for videre utvikling på
    egenhånd.

---
+++ tastaturkalkulator/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace tastaturkalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tast0 = new System.Windows.Forms.Button();
+            this.tast1 = new System.Windows.Forms.Button();
+            this.tast2 = new System.Windows.Forms.Button();
+            this.tast3 = new System.Windows.Forms.Button();
+            this.tast5 = new System.Windows.Forms.Button();
+            this.tast6 = new System.Windows.Forms.Button();
+            this.tast4 = new System.Windows.Forms.Button();
+            this.tast8 = new System.Windows.Forms.Button();
+            this.tast9 = new System.Windows.Forms.Button();
+            this.tast7 = new System.Windows.Forms.Button();
+            this.tastDele = new System.Windows.Forms.Button();
+            this.tastGange = new System.Windows.Forms.Button();
+            this.tastMinus = new System.Windows.Forms.Button();
+            this.tastPluss = new System.Windows.Forms.Button();
+            this.tastErLik = new System.Windows.Forms.Button();
+            this.tastSlett = new System.Windows.Forms.Button();
+            this.tastKomma = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.TextBox();
+            this.tastSlettSiste = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tast0
+            // 
+            this.tast0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast0.Location = new System.Drawing.Point(13, 277);
+            this.tast0.Name = "tast0";
+            this.tast0.Size = new System.Drawing.Size(50, 50);
+            this.tast0.TabIndex = 0;
+            this.tast0.Text = "0";
+            this.tast0.UseVisualStyleBackColor = true;
+            this.tast0.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast1
+            // 
+            this.tast1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast1.Location = new System.Drawing.Point(13, 221);
+            this.tast1.Name = "tast1";
+            this.tast1.Size = new System.Drawing.Size(50, 50);
+            this.tast1.TabIndex = 1;
+            this.tast1.Text = "1";
+            this.tast1.UseVisualStyleBackColor = true;
+            this.tast1.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast2
+            // 
+            this.tast2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast2.Location = new System.Drawing.Point(69, 221);
+            this.tast2.Name = "tast2";
+            this.tast2.Size = new System.Drawing.Size(50, 50);
+            this.tast2.TabIndex = 3;
+            this.tast2.Text = "2";
+            this.tast2.UseVisualStyleBackColor = true;
+            this.tast2.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast3
+            // 
+            this.tast3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast3.Location = new System.Drawing.Point(125, 221);
+            this.tast3.Name = "tast3";
+            this.tast3.Size = new System.Drawing.Size(50, 50);
+            this.tast3.TabIndex = 2;
+            this.tast3.Text = "3";
+            this.tast3.UseVisualStyleBackColor = true;
+            this.tast3.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast5
+            // 
+            this.tast5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast5.Location = new System.Drawing.Point(69, 165);
+            this.tast5.Name = "tast5";
+            this.tast5.Size = new System.Drawing.Size(50, 50);
+            this.tast5.TabIndex = 6;
+            this.tast5.Text = "5";
+            this.tast5.UseVisualStyleBackColor = true;
+            this.tast5.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast6
+            // 
+            this.tast6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast6.Location = new System.Drawing.Point(125, 165);
+            this.tast6.Name = "tast6";
+            this.tast6.Size = new System.Drawing.Size(50, 50);
+            this.tast6.TabIndex = 5;
+            this.tast6.Text = "6";
+            this.tast6.UseVisualStyleBackColor = true;
+            this.tast6.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast4
+            // 
+            this.tast4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast4.Location = new System.Drawing.Point(13, 165);
+            this.tast4.Name = "tast4";
+            this.tast4.Size = new System.Drawing.Size(50, 50);
+            this.tast4.TabIndex = 4;
+            this.tast4.Text = "4";
+            this.tast4.UseVisualStyleBackColor = true;
+            this.tast4.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast8
+            // 
+            this.tast8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast8.Location = new System.Drawing.Point(69, 109);
+            this.tast8.Name = "tast8";
+            this.tast8.Size = new System.Drawing.Size(50, 50);
+            this.tast8.TabIndex = 9;
+            this.tast8.Text = "8";
+            this.tast8.UseVisualStyleBackColor = true;
+            this.tast8.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast9
+            // 
+            this.tast9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast9.Location = new System.Drawing.Point(125, 109);
+            this.tast9.Name = "tast9";
+            this.tast9.Size = new System.Drawing.Size(50, 50);
+            this.tast9.TabIndex = 8;
+            this.tast9.Text = "9";
+            this.tast9.UseVisualStyleBackColor = true;
+            this.tast9.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tast7
+            // 
+            this.tast7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tast7.Location = new System.Drawing.Point(13, 109);
+            this.tast7.Name = "tast7";
+            this.tast7.Size = new System.Drawing.Size(50, 50);
+            this.tast7.TabIndex = 7;
+            this.tast7.Text = "7";
+            this.tast7.UseVisualStyleBackColor = true;
+            this.tast7.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tastDele
+            // 
+            this.tastDele.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tastDele.Location = new System.Drawing.Point(181, 53);
+            this.tastDele.Name = "tastDele";
+            this.tastDele.Size = new System.Drawing.Size(50, 50);
+            this.tastDele.TabIndex = 13;
+            this.tastDele.Text = "/";
+            this.tastDele.UseVisualStyleBackColor = true;
+            this.tastDele.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tastGange
+            // 
+            this.tastGange.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tastGange.Location = new System.Drawing.Point(181, 109);
+            this.tastGange.Name = "tastGange";
+            this.tastGange.Size = new System.Drawing.Size(50, 50);
+            this.tastGange.TabIndex = 12;
+            this.tastGange.Text = "*";
+            this.tastGange.UseVisualStyleBackColor = true;
+            this.tastGange.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tastMinus
+            // 
+            this.tastMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tastMinus.Location = new System.Drawing.Point(181, 165);
+            this.tastMinus.Name = "tastMinus";
+            this.tastMinus.Size = new System.Drawing.Size(50, 50);
+            this.tastMinus.TabIndex = 11;
+            this.tastMinus.Text = "-";
+            this.tastMinus.UseVisualStyleBackColor = true;
+            this.tastMinus.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tastPluss
+            // 
+            this.tastPluss.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tastPluss.Location = new System.Drawing.Point(181, 221);
+            this.tastPluss.Name = "tastPluss";
+            this.tastPluss.Size = new System.Drawing.Size(50, 50);
+            this.tastPluss.TabIndex = 10;
+            this.tastPluss.Text = "+";
+            this.tastPluss.UseVisualStyleBackColor = true;
+            this.tastPluss.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tastErLik
+            // 
+            this.tastErLik.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tastErLik.Location = new System.Drawing.Point(125, 277);
+            this.tastErLik.Name = "tastErLik";
+            this.tastErLik.Size = new System.Drawing.Size(106, 50);
+            this.tastErLik.TabIndex = 14;
+            this.tastErLik.Text = "=";
+            this.tastErLik.UseVisualStyleBackColor = true;
+            this.tastErLik.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tastSlett
+            // 
+            this.tastSlett.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tastSlett.Location = new System.Drawing.Point(69, 53);
+            this.tastSlett.Name = "tastSlett";
+            this.tastSlett.Size = new System.Drawing.Size(106, 50);
+            this.tastSlett.TabIndex = 15;
+            this.tastSlett.Text = "C";
+            this.tastSlett.UseVisualStyleBackColor = true;
+            this.tastSlett.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // tastKomma
+            // 
+            this.tastKomma.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tastKomma.Location = new System.Drawing.Point(69, 277);
+            this.tastKomma.Name = "tastKomma";
+            this.tastKomma.Size = new System.Drawing.Size(50, 50);
+            this.tastKomma.TabIndex = 16;
+            this.tastKomma.Text = ",";
+            this.tastKomma.UseVisualStyleBackColor = true;
+            this.tastKomma.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // display
+            // 
+            this.display.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display.Location = new System.Drawing.Point(13, 12);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(218, 35);
+            this.display.TabIndex = 17;
+            // 
+            // tastSlettSiste
+            // 
+            this.tastSlettSiste.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tastSlettSiste.Location = new System.Drawing.Point(13, 53);
+            this.tastSlettSiste.Name = "tastSlettSiste";
+            this.tastSlettSiste.Size = new System.Drawing.Size(50, 50);
+            this.tastSlettSiste.TabIndex = 18;
+            this.tastSlettSiste.Text = "←";
+            this.tastSlettSiste.UseVisualStyleBackColor = true;
+            this.tastSlettSiste.Click += new System.EventHandler(this.tast_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 340);
+            this.Controls.Add(this.tastSlettSiste);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.tastKomma);
+            this.Controls.Add(this.tastSlett);
+            this.Controls.Add(this.tastErLik);
+            this.Controls.Add(this.tastDele);
+            this.Controls.Add(this.tastGange);
+            this.Controls.Add(this.tastMinus);
+            this.Controls.Add(this.tastPluss);
+            this.Controls.Add(this.tast8);
+            this.Controls.Add(this.tast9);
+            this.Controls.Add(this.tast7);
+            this.Controls.Add(this.tast5);
+            this.Controls.Add(this.tast6);
+            this.Controls.Add(this.tast4);
+            this.Controls.Add(this.tast2);
+            this.Controls.Add(this.tast3);
+            this.Controls.Add(this.tast1);
+            this.Controls.Add(this.tast0);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tast0;
+        private System.Windows.Forms.Button tast1;
+        private System.Windows.Forms.Button tast2;
+        private System.Windows.Forms.Button tast3;
+        private System.Windows.Forms.Button tast5;
+        private System.Windows.Forms.Button tast6;
+        private System.Windows.Forms.Button tast4;
+        private System.Windows.Forms.Button tast8;
+        private System.Windows.Forms.Button tast9;
+        private System.Windows.Forms.Button tast7;
+        private System.Windows.Forms.Button tastDele;
+        private System.Windows.Forms.Button tastGange;
+        private System.Windows.Forms.Button tastMinus;
+        private System.Windows.Forms.Button tastPluss;
+        private System.Windows.Forms.Button tastErLik;
+        private System.Windows.Forms.Button tastSlett;
+        private System.Windows.Forms.Button tastKomma;
+        private System.Windows.Forms.TextBox display;
+        private System.Windows.Forms.Button tastSlettSiste;
+    }
+}
+
---
+++ tastaturkalkulator/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tastaturkalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string regneOp = "";
+        string tall1, tall2 = "0";
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.D0: { tast0.PerformClick(); break; }
+                case Keys.NumPad0: { tast0.PerformClick(); break; }
+                case Keys.D1: { tast1.PerformClick(); break; }
+                case Keys.D2: { tast2.PerformClick(); break; }
+                case Keys.D3: { tast3.PerformClick(); break; }
+                case Keys.D4: { tast4.PerformClick(); break; }
+                case Keys.D5: { tast5.PerformClick(); break; }
+                case Keys.D6: { tast6.PerformClick(); break; }
+                case Keys.D7: { tast7.PerformClick(); break; }
+                case Keys.D8: { tast8.PerformClick(); break; }
+                case Keys.D9: { tast9.PerformClick(); break; }
+                case Keys.Oemcomma: { tastKomma.PerformClick(); break; }
+            }
+        }
+
+        private void tast_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            //display.Text = b.Text;
+            switch (b.Text)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    {
+                        if (display.Text == "0")
+                            display.Text = b.Text;
+                        else
+                            display.Text += b.Text;
+                        break;
+                    }
+                case ",":
+                    {
+                        if (!display.Text.Contains(','))
+                            display.Text += ",";
+                        break;
+                    }
+                case "←":
+                    {
+                        if (display.Text.Length > 1)
+                            display.Text = display.Text.Remove(display.Text.Length - 1);
+                        else
+                            display.Text = "0";
+                        break;
+                    }
+                case "C":
+                    {
+                        display.Text = "0";
+                        regneOp = "";
+                        tall1 = tall2 = "0";
+                        break;
+                    }
+                case "=":
+                    {
+                        tall2 = display.Text;
+                        display.Text = svar(tall1, tall2, regneOp);
+                        break;
+                    }
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    {
+                        regneOp = b.Text;
+                        tall1 = display.Text;
+                        display.Text = "0";
+                        break;
+                    }
+            }
+        }
+
+        private string svar(string t1, string t2, string op)
+        {
+            string s;
+            decimal n1 = Convert.ToDecimal(t1);
+            decimal n2 = Convert.ToDecimal(t2);
+
+            if (op == "+")
+                s = Convert.ToString(n1 + n2);
+            else if (op == "-")
+                s = Convert.ToString(n1 - n2);
+            else if (op == "*")
+                s = Convert.ToString(n1 * n2);
+            else if (op == "/")
+            {
+                if (n2 == 0)
+                    s = "Kan ikke dele på null!";
+                else
+                    s = Convert.ToString(n1 / n2);
+            }
+            else
+                s = "en feil har oppstått";
+            return s;
+        }
+    }
+}
